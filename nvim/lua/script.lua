915f60d49cd5a11a924a7e969c4658e3ab8ab475
Kosmos <wkcosmology@gmail.com>
2021-03-10 14:32:00 -0500

    Fugitive config: make Gvdiff unmodifiable

--- nvim/lua/script.lua
+++ nvim/lua/script.lua
@@ -1,6 +1,10 @@
 M = {}
 
 function M.toggle_git_diff()
+    if vim.fn["FugitiveHead"]() == "" then
+        print("This is not a git repo!")
+        return
+    end
     local opened = false
     for _, v in ipairs(vim.api.nvim_list_bufs()) do
         if vim.api.nvim_buf_is_loaded(v) and string.sub(vim.fn.bufname(v), 1, 12) == 'fugitive:///' then
@@ -9,7 +13,7 @@ function M.toggle_git_diff()
         end
     end
     if opened == false then
-        vim.api.nvim_command('Gvdiffsplit!')
+        vim.api.nvim_command('Gvdiffsplit HEAD~0:%')
     end
 end
 
