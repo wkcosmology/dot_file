e90a0e044147864c82cd878b0f81b8a59303ee1a
Kai Wang <wkcosmology@gmail.com>
2024-04-22 21:55:44 +0800

    update neotree and others

--- nvim/lua/neo_tree.lua
+++ nvim/lua/neo_tree.lua
@@ -2,10 +2,10 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+vim.fn.sign_define("DiagnosticSignError", { text = "xx", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "!!", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "~~", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = ">>", texthl = "DiagnosticSignHint" })
 -- in the form "LspDiagnosticsSignWarning"
 
 require("neo-tree").setup({
@@ -37,28 +37,28 @@ require("neo-tree").setup({
       padding = 1, -- extra padding on left hand side
       -- indent guides
       with_markers = true,
-      indent_marker = "│",
-      last_indent_marker = "└",
+      indent_marker = "|",
+      last_indent_marker = "+",
       highlight = "NeoTreeIndentMarker",
       -- expander config, needed for nesting files
       with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
-      expander_collapsed = "",
-      expander_expanded = "",
+      expander_collapsed = ">",
+      expander_expanded = "-",
       expander_highlight = "NeoTreeExpander",
     },
     icon = {
-      folder_closed = "",
-      folder_open = "",
-      folder_empty = "ﰊ",
+      folder_closed = "[+]",
+      folder_open = "[-]",
+      folder_empty = "[ ]",
       -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
       -- then these will never be used.
       default = "*",
       highlight = "NeoTreeFileIcon",
     },
-    modified = {
-      symbol = "[+]",
-      highlight = "NeoTreeModified",
-    },
+    -- modified = {
+    --   symbol = "~~",
+    --   highlight = "NeoTreeModified",
+    -- },
     name = {
       trailing_slash = false,
       use_git_status_colors = true,
@@ -67,16 +67,16 @@ require("neo-tree").setup({
     git_status = {
       symbols = {
         -- Change type
-        added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-        modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-        deleted = "",
-        renamed = "➜",
+        added = "+", -- or "", but this is redundant info if you use git_status_colors on the name
+        modified = "~", -- or "", but this is redundant info if you use git_status_colors on the name
+        deleted = "-",
+        renamed = "~",
         -- Status type
-        untracked = "★",
-        ignored = "◌",
-        unstaged = "✗",
-        staged = "✓",
-        conflict = "",
+        untracked = "U",
+        ignored = "*",
+        unstaged = "U",
+        staged = "S",
+        conflict = "X",
 
       },
     },
