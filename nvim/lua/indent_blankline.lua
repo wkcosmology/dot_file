02d5cc39f833404b394532bb877c8e964b1c8d55
Kai Wang <wkcosmology@gmail.com>
2025-03-19 09:19:46 +0000

    update indent line color

--- nvim/lua/indent_blankline.lua
+++ nvim/lua/indent_blankline.lua
@@ -1,5 +1,5 @@
 require("ibl").overwrite({
   debounce = 100,
-  indent = { char = "▏", highlight = { "Label" } },
+  indent = { char = "▍", highlight = { "MoonflyGrey39" } },
   scope = { show_start = false, show_end = false },
 })
