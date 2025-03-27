7af5b4ac86ec61ef62a82046f2602de0fcb485ef
Kai Wang <wkcosmology@gmail.com>
2025-03-27 12:27:14 +0000

    update

--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -7,14 +7,10 @@ if [ "$SENDER" = "volume_change" ]; then
   VOLUME="$INFO"
 
   case "$VOLUME" in
-    [6-9][0-9]|100) ICON="󰕾"
-    ;;
-    [3-5][0-9]) ICON="󰖀"
-    ;;
-    [1-9]|[1-2][0-9]) ICON="󰕿"
+    [1-9]|[1-9][0-9]|100) ICON="󰕾"
     ;;
     *) ICON="󰖁"
   esac
 
-  sketchybar --set "$NAME" icon="$ICON" label="$VOLUME%"
+  sketchybar --set "$NAME" icon="$ICON" label="$VOLUME%" label.width=30 label.align="left"
 fi
