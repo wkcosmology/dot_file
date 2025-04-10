3006748dcb150930549fbf5cd9c4a0975a9f49f6
Kai Wang <wkcosmology@gmail.com>
2025-04-10 14:06:15 +0100

    update

--- sketchybar/plugins/take_a_break.sh
+++ sketchybar/plugins/take_a_break.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 sketchybar --set take_a_break padding_left=5 padding_right=5 \
-        background.corner_radius=10 background.height=23 background.border_width=1 \
-        background.border_color=0xffcdd6f4 background.color=0x10cdd6f4 \
-        icon.color=0xffcdd6f4 label.color=0xffcdd6f4 \
-        width=70 \
-        label="BREAK" \
-        background.border_color=0xfff60a18 background.color=0x10cdd6f4 \
-        icon.color=0xfff60a18 label.color=0xfff60a18 icon=" "
+        background.corner_radius=10 background.height=23 background.border_width=0 \
+        background.border_color=0x00fb7602 background.color=0xff00ffff \
+        width=80  label="BREAK" align=center\
+        icon.color=0xff0000FF label.color=0xff0000FF icon=" "
--- sketchybar/plugins/take_a_break_click.sh
+++ sketchybar/plugins/take_a_break_click.sh
@@ -2,9 +2,6 @@
 
 sketchybar --set take_a_break padding_left=5 padding_right=5 \
         background.corner_radius=10 background.height=23 background.border_width=1 \
-        background.border_color=0xffcdd6f4 background.color=0x10cdd6f4 \
-        icon.color=0xffcdd6f4 label.color=0xffcdd6f4 \
-        width=70 \
-        label="WORK~" icon="󰍹 " \
+        width=80 label="WORK~" icon="󰍹 " align=center\
         background.border_color=0xffcdd6f4 background.color=0x10cdd6f4 \
         icon.color=0xffcdd6f4 label.color=0xffcdd6f4
