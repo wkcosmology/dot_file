dcc0608cda3049b190478571096c8814019fb61a
Kai Wang <wkcosmology@gmail.com>
2025-04-10 15:24:10 +0100

    refine the take a break

--- sketchybar/plugins/take_a_break.sh
+++ sketchybar/plugins/take_a_break.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 sketchybar --set take_a_break padding_left=5 padding_right=5 \
+        background.corner_radius=10 background.height=23 background.border_width=1 \
+        width=80 label="WORK~" icon="󰍹 " align=center\
+        background.border_color=0xffcdd6f4 background.color=0x10cdd6f4 \
+        icon.color=0xffcdd6f4 label.color=0xffcdd6f4
+
+sleep 1200
+
+sketchybar --set take_a_break padding_left=5 padding_right=5 \
         background.corner_radius=10 background.height=23 background.border_width=0 \
         background.border_color=0x00fb7602 background.color=0xff00ffff \
         width=80  label="BREAK" align=center\
--- sketchybar/plugins/take_a_break_click.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sketchybar --set take_a_break padding_left=5 padding_right=5 \
-        background.corner_radius=10 background.height=23 background.border_width=1 \
-        width=80 label="WORK~" icon="󰍹 " align=center\
-        background.border_color=0xffcdd6f4 background.color=0x10cdd6f4 \
-        icon.color=0xffcdd6f4 label.color=0xffcdd6f4
