305ebc44e904eda4eea48a5097d36c3a66fe6c92
Kai Wang <wkcosmology@gmail.com>
2025-06-22 09:19:28 +0100

    explicitly set the clang-format configure file

--- nvim/lua/formatter_nvim.lua
+++ nvim/lua/formatter_nvim.lua
@@ -7,7 +7,7 @@ require("formatter").setup({
       function()
         return {
           exe = "clang-format",
-          args = { "--assume-filename", vim.api.nvim_buf_get_name(0) },
+          args = { "--assume-filename", vim.api.nvim_buf_get_name(0), "-style", "file:/Users/wangk/.clang-format"},
           stdin = true,
           cwd = vim.fn.expand("%:p:h"), -- Run clang-format in cwd of the file.
         }
