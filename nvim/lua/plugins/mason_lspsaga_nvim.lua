459d0bb1d499b1844ce93dd9633656683fbd3f9e
Kai Wang <wkcosmology@gmail.com>
2025-12-31 14:08:57 +0000

    update

--- nvim/lua/plugins/mason_lspsaga_nvim.lua
+++ nvim/lua/plugins/mason_lspsaga_nvim.lua
@@ -27,14 +27,14 @@ vim.diagnostic.config({
   update_in_insert = false,
 })
 
-require("lsp_signature").setup({
-  max_width = 80,
-  bind = false,
-  hint_enable = false,
-  close_timeout = 4000,
-  hi_parameter = "LspSignatureActiveParameter",
-  always_trigger = false,
-})
+-- require("lsp_signature").setup({
+--   max_width = 80,
+--   bind = false,
+--   hint_enable = false,
+--   close_timeout = 4000,
+--   hi_parameter = "LspSignatureActiveParameter",
+--   always_trigger = false,
+-- })
 
 -- lspsaga
 require("lspsaga").setup({
