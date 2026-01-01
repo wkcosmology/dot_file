0dbadc65ad6485ee42da223c14ac6fd5dc69b3fb
Kai Wang <wkcosmology@gmail.com>
2026-01-01 20:53:31 +0000

    update aerospace style

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -23,11 +23,11 @@ display_name="$(map_label "$1")"
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     sketchybar --set $NAME label="$display_name" icon="●"  \
-        label.color=0xffcba6f7 icon.color=0xffcba6f7 \
-        background.border_color=0xffcba6f7 background.color=0x30cba6f7 \
+        label.color=0xff16105c icon.color=0xff16105c \
+        background.border_color=0xffffa500 background.color=0xffffa500 \
         background.corner_radius=10 background.height=23 background.border_width=1
 else
     sketchybar --set $NAME label="$display_name" icon="○" \
-        label.color=0xdfcba6f7 icon.color=0x8fcba6f7 \
+        label.color=0xff1bd7f5 icon.color=0xff1bd7f5 \
         background.drawing=off
 fi
