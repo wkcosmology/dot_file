e12dc255ec39dd1880452a87ee8d4d3a5f4aae79
Kai Wang <wkcosmology@gmail.com>
2026-07-18 10:27:26 +0100

    update

--- sketchybar/plugins/take_a_break.sh
+++ sketchybar/plugins/take_a_break.sh
@@ -8,6 +8,8 @@ for i in {20..1}
         then
             sketchybar --set take_a_break padding_left=5 padding_right=5 \
                 background.corner_radius=10 background.height=23 background.border_width=1 \
+                background.border_color=0x00000000 background.color=0x00000000 background.clip=0 \
+                icon.color=0xfff5b042 label.color=0xfff5b042 \
                 width=80 label="$(printf "%02d" $((i))) MINs" icon="󰍹 " align=center
             sleep 60
         else
